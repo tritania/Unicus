748691da21ab40184960913fb5c615b4661692eb
Tritania <erikwilson@magnorum.com>
2014-08-15 18:35:49 +0000

    Finished building out coins class.

--- src/main/java/org/tritania/unicus/Coins.java
+++ src/main/java/org/tritania/unicus/Coins.java
@@ -22,6 +22,14 @@ import java.util.Iterator;
 import java.util.Map;
 
 import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
+import org.bukkit.scoreboard.Scoreboard;
+import org.bukkit.scoreboard.ScoreboardManager;
+import org.bukkit.scoreboard.DisplaySlot;
+import org.bukkit.scoreboard.Objective;
+import org.bukkit.scoreboard.Score;
+import org.bukkit.ChatColor;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import org.tritania.unicus.Unicus;
 import org.tritania.unicus.utils.Log;
@@ -35,23 +43,73 @@ public class Coins
         this.un = un;
     }
     
+    public void balance (Player player)
+    {
+        ScoreboardManager mang = Bukkit.getScoreboardManager();
+        Scoreboard board = mang.getNewScoreboard();
+
+        Objective objective = board.registerNewObjective("Stats", "dummy");
+
+        objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+        objective.setDisplayName(ChatColor.BLUE +  "Coins!");
+
+        Score balance = objective.getScore(Bukkit.getOfflinePlayer("Balance: "));
+        
+        balance.setScore(un.coins.get(player.getName()));
+    }
+    
+    public void removeBoard(Player player)
+    {
+        ScoreboardManager manger = Bukkit.getScoreboardManager();
+        Scoreboard board = manger.getNewScoreboard();
+
+        player.setScoreboard(board);
+    }
+    
+    public void balanceTimer(final Player player)
+    {
+        new BukkitRunnable()
+        {
+            @Override
+            public void run()
+            {
+                removeBoard(player);
+            }
+        }.runTaskLater(un, 1200); 
+    }
+    
     public void add (Player player, int amount) 
     {
-        
+        int current = un.coins.get(player.getName());
+        un.coins.put(player.getName(), (current + amount));
     }
     
     public void remove (Player player, int amount) 
     {
-        
+        int current = un.coins.get(player.getName());
+        if (current <= amount) {
+            un.coins.put(player.getName(), 0);
+        } else {
+            un.coins.put(player.getName(), (current - amount));
+        }
     }
     
-    public void transfer (Player to, Player from, int amount)
+    public boolean transfer (Player to, Player from, int amount)
     {
-        
+        if (coinCheck(from, amount)) {
+            remove(from, amount);
+            add(to, amount);
+            return true;
+        } else {
+            return false;
+        }
     }
     
     public boolean coinCheck(Player player, int amount) 
     {
-        return true; //debugging
+        if (un.coins.get(player.getName()) < amount) {
+            return true;
+        }
+        return false;
     }
 }
